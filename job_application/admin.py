7924d74e41b9864a4407be1da716478be49d4c53
R3zn1kov <ldlreznikov@gmail.com>
2024-03-19 21:54:10 +0100

    Added Navbar, multiple pages and Django Admin dashboard

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Form
+
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", )
+    ordering = ("first_name", )
+    readonly_fields = ("occupation", )
+
+
+admin.site.register(Form, FormAdmin)
